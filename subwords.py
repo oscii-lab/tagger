79dd9d26e23ce05c5544e49cf1686a515d7de198
John DeNero <denero@berkeley.edu>
2017-03-19 07:48:54 -0700

    ensemble models

--- subwords.py
+++ subwords.py
@@ -2,7 +2,7 @@
 
 from keras.preprocessing.text import Tokenizer
 
-def map_and_subworder(texts, subword_paths, max_subwords):
+def map_and_subworder(texts, subword_paths, max_subwords, transform=None):
     """Return a subword type map & a dict from raw->subworded sentences."""
     lines = [s for p in subword_paths for s in open(p).readlines()]
     sub_texts = [s.strip('\n') for s in lines]
@@ -11,12 +11,18 @@ def map_and_subworder(texts, subword_paths, max_subwords):
     subword_map = Tokenizer(lower=False, filters='')
     subword_map.fit_on_texts(sub_texts)
 
-    sub = {raw: split_into_words(sub, max_subwords) for raw, sub in zip(texts, sub_texts)}
-    for k, v in sub.items():
-        assert len(k.split(' ')) == len(v), "{} vs {}".format(k, v)
-    return subword_map, sub
+    if transform == None:
+        transform = lambda x: x
 
-def split_into_words(subword_sent, max_subwords, delimiter='@@'):
+    def split(subword_sent):
+        return split_into_words(subword_sent, max_subwords, transform)
+
+    subworder = {raw: split(sub) for raw, sub in zip(texts, sub_texts)}
+    for k, v in subworder.items():
+        assert len(k.split(' ')) == len(v), "{} vs {}".format(k, v)
+    return subword_map, subworder
+
+def split_into_words(subword_sent, max_subwords, transform, delimiter='@@'):
     """Take a subworded sentence and build a space-separated sequence of
     subwords for each word.
 
@@ -31,6 +37,6 @@ def split_into_words(subword_sent, max_subwords, delimiter='@@'):
             word.append(s)
         else:
             word.append(s)
-            words.append(' '.join(word[:max_subwords]))
+            words.append(' '.join(transform(word)[:max_subwords]))
             word = []
     return words
--- tagger.py
+++ tagger.py
@@ -1,9 +1,8 @@
-# Convolutional tagger over subwords
+# Part-of-speech tagger
 
 import itertools
 import functools
 import json
-import os
 import shutil
 import sys
 from datetime import datetime
@@ -38,40 +37,87 @@ subword_paths = [
     'data/web_all.' + size + '.txt',
     ]
 
-if len(sys.argv) > 1:
+if len(sys.argv) > 1 and not sys.argv[1] == '-f':
     subword_paths = sys.argv[1:]
 
-max_subwords = 8
+# Subword representations; each takes a list of strings: subwords for a word.
+
+def last_delimiter(subwords):
+    if len(subwords) > 1:
+        return subwords[:-1] + ['@@' + subwords[-1]]
+    else:
+        return subwords
+
+def no_delimiter(subwords):
+    return [s.strip('@@') for s in subwords]
+
+def length(subwords):
+    length = '@@{}@@'.format(len(subwords))
+    return [length] + [s.strip('@@') for s in subwords[:max_subwords-1]]
+
+def join(subwords):
+    return [''.join([s.strip('@@') for s in subwords])]
+
+# Define two different representations of subwords using the functions above.
+
+max_subwords = 10
 print('subword_paths:', subword_paths)
-subword_map, subworder = map_and_subworder(texts, subword_paths, max_subwords)
+sub_with = lambda f: map_and_subworder(texts, subword_paths, max_subwords, f)
+subword_map1, subworder1 = sub_with(last_delimiter)
+subword_map2, subworder2 = sub_with(no_delimiter)
+
+print(subworder1[texts[0]])
+print(subworder2[texts[0]])
 
 # %%
-# Build a model.
+# Build models.
 
-num_words = len(word_map.word_index)+1
-num_subwords = len(subword_map.word_index)+1
-num_tags = len(tag_map.word_index)+1
-max_len = max(w.count(' ') + 1 for w in subworder.keys())
+num_words = len(word_map.word_index) + 1
+num_subwords1 = len(subword_map1.word_index) + 1
+num_subwords2 = len(subword_map2.word_index) + 1
+num_tags = len(tag_map.word_index) + 1
+max_len = max(w.count(' ') + 1 for w in texts)
 # max_subwords = max(w.count(' ') + 1 for s in subworder.values() for w in s)
 word_size = 64
 
-# Embed each subword
-subwords = Input(shape=(max_len, max_subwords), dtype='int32', name='Subwords')
-sub_embedding = Embedding(input_dim=num_subwords, output_dim=word_size, mask_zero=True)
+subwords1 = Input(shape=(max_len, max_subwords), dtype='int32')
+subwords2 = Input(shape=(max_len, max_subwords), dtype='int32')
 
-# Embed subword sequences each into a single word vector.
-embedded = TimeDistributed(LSTM(word_size))(TimeDistributed(sub_embedding)(subwords))
+def make(drop1, drop2):
+    # Embed each subword
+    embed1 = Embedding(input_dim=num_subwords1, output_dim=word_size, mask_zero=True, dropout=drop1)
+    embed2 = Embedding(input_dim=num_subwords2, output_dim=word_size, mask_zero=True, dropout=drop2)
+    embedded1 = TimeDistributed(embed1)(subwords1)
+    embedded2 = TimeDistributed(embed2)(subwords2)
 
-# Build a convolutional network
-convolved = Convolution1D(word_size, 5, border_mode='same')(embedded)
-representations = [embedded, convolved]
-merged = merge(representations, mode='sum')
-tags = Dense(num_tags, activation='softmax', name='Tag')(merged)
+    # Concatenate both embeddings & embed each subword sequence into a word vector.
+    embedded12 = merge([embedded1, embedded2], mode='sum')
+    embedded = TimeDistributed(LSTM(word_size))(embedded12)
 
-model = Model(input=subwords, output=tags)
-model.compile(optimizer='adam',
-              loss=padded_categorical_crossentropy,
-              metrics=[padded_categorical_accuracy])
+    # Build a convolutional network
+    convolved_words = Convolution1D(word_size, 5, border_mode='same')(embedded)
+    merged_words = merge([embedded, convolved_words], mode='sum')
+
+    # Predict tags from words
+    tagger = Dense(num_tags, activation='softmax')
+    tagged = tagger(merged_words)
+
+    # Predict tags again from words and tags
+    convolved_tags = Convolution1D(word_size, 5, border_mode='same')(tagged)
+    merged = merge([merged_words, convolved_tags], mode='sum')
+    tags = tagger(merged)
+
+    model = Model(input=[subwords1, subwords2], output=tags)
+    model.compile(optimizer='adam',
+                  loss=padded_categorical_crossentropy,
+                  metrics=[padded_categorical_accuracy])
+    return model, tags
+
+# Note: Put just one (label, model) pair in models for a simple experiment.
+models = []
+drops = [[0, 0], [0.1, 0.1], [0.2, 0.2], [0.2, 0.1]]
+for drop in drops:
+    models.append((str(drop), make(*drop)))
 
 # %%
 # Prepare data format for model.
@@ -85,13 +131,16 @@ def tag_string(tagged):
 def prep(tagged_sents):
     """Convert a dataset of tagged sentences into inputs and outputs."""
     tagged_sents = list(tagged_sents) # because we'll iterate twice
-    x = np.array([prep_subword(word_string(t)) for t in tagged_sents])
+    one, two = [(subword_map1, subworder1), (subword_map2, subworder2)]
+    x1 = np.array([prep_subword(word_string(t), *one) for t in tagged_sents])
+    x2 = np.array([prep_subword(word_string(t), *two) for t in tagged_sents])
+    x = [x1, x2]
     tags = tag_map.texts_to_sequences(map(tag_string, tagged_sents))
     padded_tags = sequence.pad_sequences(tags, maxlen=max_len, value=0)
     y = np.array([np_utils.to_categorical(t, num_tags) for t in padded_tags])
     return x, y
 
-def prep_subword(sentence):
+def prep_subword(sentence, subword_map, subworder):
     """Convert sentence into a padded array of subword embeddings."""
     subs = subword_map.texts_to_sequences(subworder[sentence])
     padded_subs = sequence.pad_sequences(subs, maxlen=max_subwords, value=0)
@@ -104,43 +153,58 @@ test = prep(tagged_sents([ptb_test]))
 web_tests = [prep(tagged_sents([w])) for w in web_all]
 
 # %%
+# Train and evaluate.
 
 early_stopping = EarlyStopping(monitor='val_padded_categorical_accuracy',
                                min_delta=0.0005, patience=0, verbose=1)
-history = model.fit(x, y, batch_size=32, nb_epoch=100, verbose=1,
-                    validation_data=val, callbacks=[early_stopping])
+def train(model):
+    return model.fit(x, y, batch_size=32, nb_epoch=4, verbose=1,
+                     validation_data=val, callbacks=[early_stopping]).history
 
-losses = []
-accs = []
-for name, data in zip(['val', 'test'] + web_genres, [val, test] + web_tests):
-    loss = model.evaluate(*data, verbose=2)
-    losses.append((name, loss))
-    accs.append('{:0.4f}'.format(loss[1]))
-    print('{}: loss: {:0.4f} - acc: {:0.4f}'.format(name, *loss))
+def evaluate(label, model, history, exp_dir='exp2'):
+    """Evaluate a labeled model on all test sets and save it."""
+    print('Evaluating', label)
+    losses = []
+    accs = []
+    for name, data in zip(['val', 'test'] + web_genres, [val, test] + web_tests):
+        loss = model.evaluate(*data, verbose=2)
+        losses.append((name, loss))
+        accs.append('{:0.4f}'.format(loss[1]))
+        print('{}: loss: {:0.4f} - acc: {:0.4f}'.format(name, *loss))
 
-print('\t'.join(accs))
+    print('\t'.join(accs)) # For easy spreadsheet copy/paste
 
+    output_dir = datetime.today().strftime(exp_dir + '/%y%m%d_%H%M%S')
+    if not os.path.exists(exp_dir):
+        os.mkdir(exp_dir)
+    os.mkdir(output_dir)
 
-# %%
-# Save everything
+    try:
+        with open(output_dir + '/model.json', 'w') as jout:
+            jout.write(model.to_json())
+        model.save(output_dir + '/model.h5')
+    except:
+        pass
 
-output_dir = datetime.today().strftime('exp/%y%m%d_%H%M%S')
+    with open(output_dir + '/info.json', 'w') as jout:
+        info = {
+            'history': history,
+            'losses': losses,
+            'sys.argv': sys.argv,
+            'label': label,
+        }
+        json.dump(info, jout, indent=2)
 
-if not os.path.exists('exp'):
-    os.mkdir('exp')
-os.mkdir(output_dir)
+    shutil.copyfile('tagger.py', output_dir + '/tagger.py')
 
-with open(output_dir + '/model.json', 'w') as jout:
-    jout.write(model.to_json())
+for label, (model, _) in models:
+    history = train(model)
+    evaluate(label, model, history)
 
-model.save(output_dir + '/model.h5')
-
-with open(output_dir + '/info.json', 'w') as jout:
-    info = {
-        'history': history.history,
-        'losses': losses,
-        'sys.argv': sys.argv
-    }
-    json.dump(info, jout, indent=2)
-
-shutil.copyfile('tagger.py', output_dir + '/tagger.py')
+if len(models) > 1:
+    ensemble = merge([tags for (_, (_, tags)) in models], mode='ave')
+    model = Model(input=[subwords1, subwords2], output=ensemble)
+    model.compile(optimizer='adam',
+                  loss=padded_categorical_crossentropy,
+                  metrics=[padded_categorical_accuracy])
+    evaluate('ensemble', model, None)
