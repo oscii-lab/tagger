3374475396ba1c04323f1f979860e56ac8005577
John DeNero <denero@berkeley.edu>
2017-03-19 19:42:13 -0700

    tagger that runs repeated experiments

--- tagger.py
+++ tagger.py
@@ -41,84 +41,64 @@ subword_paths = [
 if len(sys.argv) > 1 and not sys.argv[1] == '-f':
     subword_paths = sys.argv[1:]
 
-# Subword representations; each takes a list of strings: subwords for a word.
-
-def last_delimiter(subwords):
-    if len(subwords) > 1:
-        return subwords[:-1] + ['@@' + subwords[-1]]
-    else:
-        return subwords
-
-def no_delimiter(subwords):
-    return [s.strip('@@') for s in subwords]
-
-def length(subwords):
-    length = '@@{}@@'.format(len(subwords))
-    return [length] + [s.strip('@@') for s in subwords[:max_subwords-1]]
-
-def join(subwords):
-    return [''.join([s.strip('@@') for s in subwords])]
-
-# Define two different representations of subwords using the functions above.
-
 max_subwords = 10
 print('subword_paths:', subword_paths)
-sub_with = lambda f: map_and_subworder(texts, subword_paths, max_subwords, f)
-subword_map1, subworder1 = sub_with(last_delimiter)
-subword_map2, subworder2 = sub_with(no_delimiter)
+subword_map, subworder = map_and_subworder(texts, subword_paths, max_subwords)
 
-print(subworder1[texts[0]])
-print(subworder2[texts[0]])
+print(subworder[texts[0]])
 
 # %%
 # Build models.
 
 num_words = len(word_map.word_index) + 1
-num_subwords1 = len(subword_map1.word_index) + 1
-num_subwords2 = len(subword_map2.word_index) + 1
+num_subwords = len(subword_map.word_index) + 1
 num_tags = len(tag_map.word_index) + 1
 max_len = max(w.count(' ') + 1 for w in texts)
 # max_subwords = max(w.count(' ') + 1 for s in subworder.values() for w in s)
 word_size = 64
 
-subwords1 = Input(shape=(max_len, max_subwords), dtype='int32')
-subwords2 = Input(shape=(max_len, max_subwords), dtype='int32')
+subwords = Input(shape=(max_len, max_subwords), dtype='int32')
 
-def make(drop1, drop2):
-    # Embed each subword
-    embed1 = Embedding(input_dim=num_subwords1, output_dim=word_size, mask_zero=True, dropout=drop1)
-    embed2 = Embedding(input_dim=num_subwords2, output_dim=word_size, mask_zero=True, dropout=drop2)
-    embedded1 = TimeDistributed(embed1)(subwords1)
-    embedded2 = TimeDistributed(embed2)(subwords2)
+def make(dropout=0, k=1, tag_twice=False):
+    # Embed each subword k times
+    embedded_subwords_list = []
+    for _ in range(k):
+        e = Embedding(input_dim=num_subwords, output_dim=word_size, mask_zero=True, dropout=dropout)
+        embedded_subwords_list.append(TimeDistributed(e)(subwords))
+    if k == 1:
+        embedded_subwords = embedded_subwords_list[0]
+    else:
+        embedded_subwords = merge(embedded_subwords_list, mode='sum')
 
-    # Concatenate both embeddings & embed each subword sequence into a word vector.
-    embedded12 = merge([embedded1, embedded2], mode='sum')
-    embedded = TimeDistributed(LSTM(word_size))(embedded12)
+    # Embed each subword sequence into a word vector.
+    embedded_words = TimeDistributed(LSTM(word_size))(embedded_subwords)
 
     # Build a convolutional network
-    convolved_words = Convolution1D(word_size, 5, border_mode='same')(embedded)
-    merged_words = merge([embedded, convolved_words], mode='sum')
+    convolved_words = Convolution1D(word_size, 5, border_mode='same')(embedded_words)
+    merged_words = merge([embedded_words, convolved_words], mode='sum')
 
     # Predict tags from words
     tagger = Dense(num_tags, activation='softmax')
-    tagged = tagger(merged_words)
+    tags = tagger(merged_words)
 
     # Predict tags again from words and tags
-    convolved_tags = Convolution1D(word_size, 5, border_mode='same')(tagged)
-    merged = merge([merged_words, convolved_tags], mode='sum')
-    tags = tagger(merged)
+    if tag_twice:
+        convolved_tags = Convolution1D(word_size, 5, border_mode='same')(tags)
+        merged = merge([merged_words, convolved_tags], mode='sum')
+        tags = tagger(merged)
 
-    model = Model(input=[subwords1, subwords2], output=tags)
+    model = Model(input=subwords, output=tags)
     model.compile(optimizer='adam',
                   loss=padded_categorical_crossentropy,
                   metrics=[padded_categorical_accuracy])
     return model, tags
 
 # Note: Put just one (label, model) pair in models for a simple experiment.
-models = []
-drops = [[0, 0], [0.1, 0.1], [0.2, 0.2], [0.2, 0.1]]
-for drop in drops:
-    models.append((str(drop), make(*drop)))
+repetitions = range(8)
+models = [(str([0.0, k, 'tag_once']), make(0.0, 1, False)) for k in repetitions]
+models = [(str([0.2, k, 'tag_once']), make(0.2, 2, False)) for k in repetitions]
+models = [(str([0.0, k, 'tag_twice']), make(0.0, 1, True)) for k in repetitions]
+models = [(str([0.2, k, 'tag_twice']), make(0.2, 2, True)) for k in repetitions]
 
 # %%
 # Prepare data format for model.
@@ -132,10 +112,8 @@ def tag_string(tagged):
 def prep(tagged_sents):
     """Convert a dataset of tagged sentences into inputs and outputs."""
     tagged_sents = list(tagged_sents) # because we'll iterate twice
-    one, two = [(subword_map1, subworder1), (subword_map2, subworder2)]
-    x1 = np.array([prep_subword(word_string(t), *one) for t in tagged_sents])
-    x2 = np.array([prep_subword(word_string(t), *two) for t in tagged_sents])
-    x = [x1, x2]
+    subs = (subword_map, subworder)
+    x = np.array([prep_subword(word_string(t), *subs) for t in tagged_sents])
     tags = tag_map.texts_to_sequences(map(tag_string, tagged_sents))
     padded_tags = sequence.pad_sequences(tags, maxlen=max_len, value=0)
     y = np.array([np_utils.to_categorical(t, num_tags) for t in padded_tags])
@@ -204,7 +182,7 @@ for label, (model, _) in models:
 
 if len(models) > 1:
     ensemble = merge([tags for (_, (_, tags)) in models], mode='ave')
-    model = Model(input=[subwords1, subwords2], output=ensemble)
+    model = Model(input=subwords, output=ensemble)
     model.compile(optimizer='adam',
                   loss=padded_categorical_crossentropy,
                   metrics=[padded_categorical_accuracy])
