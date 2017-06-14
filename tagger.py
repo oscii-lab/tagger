52964983112e1fe4af30bbd986cc6e135a4e0a14
naltieri <nl.altieri@gmail.com>
2017-06-14 12:12:15 -0700

    positional information added to tagger

--- tagger.py
+++ tagger.py
@@ -28,6 +28,7 @@ from keras import backend as K
 from loss import *
 from tags import *
 from subwords import *
+from transformer import *
 
 print('Vocab size:', len(word_map.word_counts))
 print('Tag set size:', len(tag_map.word_counts))
@@ -52,7 +53,7 @@ args = parser.parse_args()
 
 
 if args.subword_paths is not None:
-    subword_paths = options.subword_paths
+    subword_paths[0] = args.subword_paths
 
 
 
@@ -77,6 +78,9 @@ char_size = 50
 
 
 subwords = Input(shape=(max_len, max_subwords), dtype='int32')
+word_positions = Input(shape=(max_len,word_dim), dtype='float32')
+
+
 
 def encoder(x):
     if args.encoder == "conv":
@@ -106,20 +110,30 @@ def make(dropout=0, k=1, tag_twice=False):
 
     # Embed each subword sequence into a word vector.
     bi_embedded_words = TimeDistributed(Bidirectional(LSTM(lstm_size)))(embedded_subwords)
-    embedded_words = TimeDistributed(Dense(word_dim))(bi_embedded_words)
+    embedded_words = merge([TimeDistributed(Dense(word_dim))(bi_embedded_words),word_positions],mode='sum')
+
+    # embedded_words = embedded_subwords
+    # for i in range(3):
+    #     embedded_words = TimeDistributed(Transformer(1024, residual=i>0))(embedded_words)
+    #     embedded_words = TimeDistributed(BatchNormalization())(embedded_words)
 
     # Build a convolutional network
-    bi_encoded_words = encoder(embedded_words)
-    encoded_words = TimeDistributed(Dense(lstm_size,activation='tanh'))(bi_encoded_words)
+    encoded_words = embedded_words
+    for i in range(3):
+        encoded_words = Transformer(150, residual=i>0)(encoded_words)
+        encoded_words = BatchNormalization()(encoded_words)
+
+    # bi_encoded_words = encoder(embedded_words)
+    # encoded_words = TimeDistributed(Dense(lstm_size,activation='tanh'))(bi_encoded_words)
 
     # Predict tags from words
     tagger = Dense(num_tags, activation='softmax')
     tags = tagger(encoded_words)
 
     sgd = optimizers.SGD(lr=0.2, momentum=0.95)
-    model = Model(input=subwords, output=tags)
+    model = Model(input=[subwords,word_positions], output=tags)
 
-    model.compile(optimizer=sgd,
+    model.compile(optimizer='adam',
                   loss=padded_categorical_crossentropy,
                   metrics=[padded_categorical_accuracy])
     return model, tags
@@ -158,9 +172,37 @@ def prep_subword(sentence, subword_map, subworder):
     padding = np.zeros([max_len-len(subs), max_subwords])
     return np.append(padding, padded_subs, axis=0).astype(np.int32)
 
+def create_positional_embeddings(s,embed_dim):
+    assert len(s) == 2
+    def pos_to_sin_cos(pos,n_dims):
+        new_arr = np.zeros(n_dims)
+        for i in range(n_dims):
+            if i % 2 == 0:
+                new_arr[i] = np.sin((pos/10000)**(2*i/n_dims))
+            else:
+                new_arr[i] = np.cos((pos/10000)**(2*i/n_dims))
+        return new_arr
+    max_pos = s[1]
+    pos_arrs = []
+    for pos in np.arange(max_pos):
+        pos_arrs += [pos_to_sin_cos(pos,embed_dim)]
+    pos_arrs = np.array(pos_arrs)
+
+    single_pos_embed = np.reshape(pos_arrs,[1,pos_arrs.shape[0],pos_arrs.shape[1]])
+    pos_embeds = np.tile(single_pos_embed,[s[0],1,1])
+    assert pos_embeds.shape == (s[0],s[1],embed_dim)
+    assert np.all(pos_embeds[0][2] == pos_to_sin_cos(2,embed_dim))
+    return pos_embeds
+
+
 x, y = prep(tagged_sents([ptb_train]))
+tr_pos = create_positional_embeddings(x.shape[:2],word_dim)
 val = prep(tagged_sents([ptb_dev]))
+val_pos = create_positional_embeddings(val[0].shape[:2],word_dim)
+val = [[val[0],val_pos],val[1]]
 test = prep(tagged_sents([ptb_test]))
+test_pos = create_positional_embeddings(test[0].shape[:2],word_dim)
+test = [[test[0],test_pos],test[1]]
 web_tests = [prep(tagged_sents([w])) for w in web_all]
 
 # %%
@@ -169,7 +211,7 @@ web_tests = [prep(tagged_sents([w])) for w in web_all]
 early_stopping = EarlyStopping(monitor='val_padded_categorical_accuracy',
                                min_delta=0.0005, patience=0, verbose=1)
 def train(model):
-    return model.fit(x, y, batch_size=100, nb_epoch=32, verbose=1,
+    return model.fit([x,tr_pos], y, batch_size=100, nb_epoch=32, verbose=1,
                      validation_data=val, callbacks=[early_stopping]).history
 
 def evaluate(label, model, history, exp_dir='exp'):
