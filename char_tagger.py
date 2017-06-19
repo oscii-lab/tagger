convolutional encoder and fix for layer normalization

char_tagger.py
@@ -46,32 +46,27 @@ print('Tag set size: ', num_tags-1)
 # Build models.
 
 max_word_len = 20
-lstm_size = 150
+lstm_size = 50
 char_size = 50
 word_size = 128
 model_type = ['lstm', 'transformer'][1]
-num_layers = 6
+char_encoder = ['lstm', 'conv'][1]
+
+num_layers = 3
 
 chars = Input(shape=(None, max_word_len), dtype='int32')
 
-
-def LayerNormalize(inputs):
-    g = tf.get_variable(
-            name="g",
-            shape=[inputs.get_shape()[2]],
-            initializer=tf.constant_initializer(0.1))
-    b = tf.get_variable(
-            name="b",
-            shape=[inputs.get_shape()[2]],
-            initializer=tf.constant_initializer(0))
-
-    mean = tf.reduce_mean(inputs, -1, keep_dims=True)
-
-    deviation = inputs - mean
-    x = tf.square(deviation)
-    stdDev = tf.sqrt(tf.reduce_mean(x, axis = 2, keep_dims=True))
-
-    return (g / stdDev) * deviation + b
+#
+# def LayerNormalize(inputs):
+#     g = Dense(word_size)(K.constant(0,shape=(word_size,)))
+#     b = Dense(word_size)(K.constant(0,shape=(word_size,)))
+#     mean = tf.reduce_mean(inputs, -1, keep_dims=True)
+#
+#     deviation = inputs - mean
+#     x = tf.square(deviation)
+#     stdDev = tf.sqrt(tf.reduce_mean(x, axis = 2, keep_dims=True))
+#
+#     return (g / stdDev) * deviation + b
 
 def create_model():
     """Return a model."""
@@ -81,8 +76,20 @@ def create_model():
     embedded_chars = TimeDistributed(char_embedding)(chars)
 
     char_context = Bidirectional(LSTM(lstm_size))
+
     word_encoder = Dense(word_size)
-    embedded_words = word_encoder(TimeDistributed(char_context)(embedded_chars))
+    if char_encoder == 'conv':
+        char_context_conv = Convolution1D(lstm_size*2,kernel_size=4)
+        embed_conv_words = TimeDistributed(char_context_conv)(embedded_chars)
+        embed_conv_words = Activation('relu')(embed_conv_words)
+        embedded_words = Lambda(lambda x: K.sum(x,axis = -2))(embed_conv_words)
+        embedded_words = word_encoder(embedded_words)
+    else:
+            
+        char_context = Bidirectional(LSTM(lstm_size))
+        word_encoder = Dense(word_size)
+        embedded_words = word_encoder(TimeDistributed(char_context)(embedded_chars))
+
 
     if model_type == 'lstm':
         word_context = Bidirectional(LSTM(lstm_size, return_sequences=True))
@@ -92,9 +99,6 @@ def create_model():
         embedded_contexts = Dropout(.1)(AddPositionEncodings(embedded_words))
         for i in range(num_layers):
             embedded_contexts = Transformer(word_size, residual=True)(embedded_contexts)
-            # embedded_contexts = BatchNormalization()(embedded_contexts)
-            embedded_contexts = Lambda(LayerNormalize)(embedded_contexts)
-
     # embedded_words_and_position = Dropout(.1)(AddPositionEncodings(embedded_words))
     embedded_contexts = merge([embedded_contexts,embedded_words],mode='sum')
 

transformer.py
@@ -81,12 +81,12 @@ class Transformer(Dense):
             keys = K.dot(k, kp)
             values = K.dot(v, vp)
             logits = K.batch_dot(queries, K.permute_dimensions(keys, [0, 2, 1]))
-            distributions = Dropout(.1)(K.softmax(logits / K.constant(np.sqrt(self.projected_dim))))
+            distributions = Dropout(0)(K.softmax(logits / K.constant(np.sqrt(self.projected_dim))))
             weighted_values = K.batch_dot(distributions, values)
             encodings.append(weighted_values)
         encoding = K.concatenate(encodings)
         if self.residual:
-            encoding = x + Dropout(.1)(encoding)
+            encoding = x + Dropout(0)(encoding)
         # TODO Layer normalization
 
         linear = K.dot(encoding, self.relu_kernel)